e7a68f8a8533f6dc33400d53120653c1a406d50a
Francisco Dourado <francisco.dourado@hotmail.com>
2021-09-28 21:59:20 +0100

    Update config.py

--- gestao/config.py
+++ gestao/config.py
@@ -1,6 +1,6 @@
 customhost = "gestao.ckdbsxypndwc.us-east-2.rds.amazonaws.com"
 customuser = "admin"
 custompass = "12345678"
-customdb = "gestao"
+customdb = "funcionarios"
 custombucket = "addempresa"
 customregion = "us-east-2"
